0ff30e64dcc63bb903a29d6b879e65b8bdb6c7f0
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-08-18 09:03:45 +0500

    updated

--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Header from './Basic-component/Hearder'
-
+import { MdOutlineDirectionsBike } from 'react-icons/md'
+import Product from '../Data/Product'
 const Home = () => {
     return (
         <>
             <Header />
-            <div>Home</div>
+            <div className="hero">
+                <div className="hero-details">
+                    <span>
+                        Bike Delivery
+                        <MdOutlineDirectionsBike />
+                    </span>
+                    <h1 className="hero-heading">
+                        The Fastest Delivery in Your City
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, nesciunt modi provident cupiditate veritatis non sint. Commodi consequatur quod nam doloremque odio, obcaecati porro soluta, nostrum ipsam quaerat voluptates totam.
+                    </p>
+                    <button className="btn">Order Now</button>
+                </div>
+                <div className="boxes">
+                    {Product.map((pro) => (
+                        <div className="box">
+                            <h1>{pro.name}</h1>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
---
+++ client/src/Data/Product.js
@@ -0,0 +1,10 @@
+const Product = [
+    {
+        name: "Icecream",
+        description: "Chocolate & Vanila",
+        priice: "5.25",
+        image: "./images/i1.png"
+    }
+]
+
+export default Product;
